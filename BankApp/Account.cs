8885cbfc433fd1baf263bf78ed45e73bbf690a1d
Dan Poorte <danpoo@microsoft.com>
2020-01-24 14:55:01 -0800

    enum

---
+++ BankApp/Account.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankApp
+{
+    enum TypeOfAccounts
+    {
+        Checking,
+        Savings,
+        CD,
+        Loan
+    }
+
+    /// <summary>
+    /// This class represents a bank account
+    /// where you can deposit or withdraw money from
+    /// </summary>
+    class Account
+    {
+        private static int lastAccountNumber = 0;
+
+        #region Properties
+        /// <summary>
+        /// Account number for the bank account
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        public string AccountName { get; set; }
+
+        public decimal Balance { get; private set; }
+        public TypeOfAccounts AccountType { get; set; }
+        public DateTime CreatedDate { get; private set; }
+        public string EmailAddress { get; set; }
+        #endregion
+
+        #region Methods
+       /// <summary>
+       /// Deposit money into your account
+       /// </summary>
+       /// <param name="amount">Amount to deposit</param>
+        public void Deposit(decimal amount)
+        {
+            //Balance = Balance + Amount
+            Balance += amount;
+        }
+        /// <summary>
+        /// Withdraw money from your account
+        /// </summary>
+        /// <param name="amount">Amount to withdraw</param>
+        /// <returns>New balance</returns>
+        public decimal Withdraw(decimal amount)
+        {
+            Balance -= amount;
+            return Balance;
+        }
+        #endregion
+        #region Constructor
+        //Default constructor
+        public Account()
+        {
+            //lastAccountNumber++;
+            //AccountNumber = lastAccountNumber;
+
+            AccountNumber = ++lastAccountNumber;
+            CreatedDate = DateTime.UtcNow;
+
+        }
+        #endregion
+    }
+}
---
+++ BankApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BankApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myAccount = new Account();
+            myAccount.AccountName = "My checking";
+            myAccount.Deposit(1000);
+            myAccount.AccountType = TypeOfAccounts.Checking;
+            Console.WriteLine($"AN: {myAccount.AccountNumber},Accountname: {myAccount.AccountName}, B: {myAccount.Balance:C},CD: {myAccount.CreatedDate},AT: {myAccount.AccountType}" ); 
+
+
+            var myAccount2 = new Account();
+            myAccount.AccountName = "My savings";
+            myAccount.AccountType = TypeOfAccounts.Savings;
+            Console.WriteLine($"AN: {myAccount2.AccountNumber},Accountname: {myAccount2.AccountName}, B: {myAccount2.Balance:C},CD: {myAccount2.CreatedDate},AT: {myAccount2.AccountType}");
+
+            var myAccount3 = new Account();
+            myAccount.AccountName = "My savings";
+            myAccount.AccountType = TypeOfAccounts.Savings;
+            Console.WriteLine($"AN: {myAccount3.AccountNumber},Accountname: {myAccount3.AccountName}, B: {myAccount3.Balance:C},CD: {myAccount3.CreatedDate},AT: {myAccount3.AccountType}");
+
+
+
+
+        }
+    }
+}
